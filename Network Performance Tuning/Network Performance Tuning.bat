8982477bd6dcba43146ac76e97b0afc4b420dbdc
moffa89 <85792173+moffa89@users.noreply.github.com>
2025-09-07 04:46:40 +0200

    Add files via upload

--- Network Performance Tuning/Network Performance Tuning.bat
+++ Network Performance Tuning/Network Performance Tuning.bat
@@ -13,3 +13,10 @@ reg add "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Services\Tcpip\Parameters" 
 
 echo Optimize UDP Registry
 reg add "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Services\AFD\Parameters" /v FastSendDatagramThreshold /t reg_dword /d 0 /f
+
+echo Additional TCP Optimizer Settings
+netsh int tcp set global autotuninglevel=disabled
+netsh int tcp set global ecncapability=disabled
+netsh int tcp set global rss=enabled
+netsh int tcp set global rsc=disabled
+netsh int ip set global taskoffload=disabled
