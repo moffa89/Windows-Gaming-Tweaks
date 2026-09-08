1b493928094834e9a5ce1416f428340ec1f37a6b
moffa89 <85792173+moffa89@users.noreply.github.com>
2026-09-08 21:22:07 +0200

    Add files via upload

--- Network/NetworkTweaks.bat
+++ Network/NetworkTweaks.bat
@@ -6,13 +6,12 @@ echo Looking for Admin rights
 net session >nul 2>&1 || (powershell start -verb runas '"%~0"' &exit /b)
 
 echo Optimize TCP Registry
-reg add "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Services\Tcpip\Parameters" /v TcpAckFrequency /t reg_dword /d 1 /f
-reg add "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Services\Tcpip\Parameters" /v TCPNoDelay /t reg_dword /d 1 /f
-reg add "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Services\Tcpip\Parameters" /v TcpDelAckTicks /t reg_dword /d 0 /f
-reg add "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Services\Tcpip\Parameters" /v DefaultTTL /t reg_dword /d 64 /f
+reg add "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Services\Tcpip\Parameters" /v SackOpts /t reg_dword /d 0 /f
 
 echo Optimize UDP Registry
-reg add "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Services\AFD\Parameters" /v FastSendDatagramThreshold /t reg_dword /d 0 /f
+reg add "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Services\AFD\Parameters" /v FastSendDatagramThreshold /t reg_dword /d 1492 /f
+reg add "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Services\AFD\Parameters" /v DynamicBacklogGrowthDelta /t reg_dword /d 10 /f
+reg add "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Services\AFD\Parameters" /v EnableDynamicBacklog /t reg_dword /d 1 /f
 
 echo Additional TCP Optimizer Settings
 netsh int tcp set global autotuninglevel=disabled
@@ -20,3 +19,5 @@ netsh int tcp set global ecncapability=disabled
 netsh int tcp set global rss=enabled
 netsh int tcp set global rsc=disabled
 netsh int ip set global taskoffload=disabled
+netsh int tcp set global nonsackrttresiliency=disabled
+netsh int tcp set global timestamps=disabled
