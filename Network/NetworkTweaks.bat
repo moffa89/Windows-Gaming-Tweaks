bdec49fb0bfc338fe298b539c24a3279921504d7
moffa89 <85792173+moffa89@users.noreply.github.com>
2026-09-13 14:46:14 +0200

    Add files via upload

--- Network/NetworkTweaks.bat
+++ Network/NetworkTweaks.bat
@@ -10,14 +10,12 @@ reg add "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Services\Tcpip\Parameters" 
 
 echo Optimize UDP Registry
 reg add "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Services\AFD\Parameters" /v FastSendDatagramThreshold /t reg_dword /d 1492 /f
-reg add "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Services\AFD\Parameters" /v DynamicBacklogGrowthDelta /t reg_dword /d 10 /f
-reg add "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Services\AFD\Parameters" /v EnableDynamicBacklog /t reg_dword /d 1 /f
 
 echo Additional TCP Optimizer Settings
+netsh int ip set global taskoffload=disabled
 netsh int tcp set global autotuninglevel=disabled
 netsh int tcp set global ecncapability=disabled
+netsh int tcp set global nonsackrttresiliency=disabled
 netsh int tcp set global rss=enabled
 netsh int tcp set global rsc=disabled
-netsh int ip set global taskoffload=disabled
-netsh int tcp set global nonsackrttresiliency=disabled
 netsh int tcp set global timestamps=disabled
